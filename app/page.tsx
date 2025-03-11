10c43fa4a5e9041687385827d4cb36bc93493c67
Bhavesh Singhal <work.bhavesh22@gmail.com>
2025-03-11 15:39:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,10 @@ import HeroSection from '@/components/hero-section';
 import ProjectsSection from '@/components/projects/projects-section';
 import SkillsSection from '@/components/skills/skills-section';
 import ContactSection from '@/components/contact-section';
-import ThunderText from '@/components/thunder-text';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
-      <ThunderText 
-        text="BHAVESH SINGHAL " 
-        fontSize={120}
-        fontFamily='GodOfThunder'
-        textColor="#FFA500"
-        thunderColor="#FFD700"
-        thunderGlow="#FFD700"
-      />
       <NavHeader />
       <HeroSection />
       <ProjectsSection />
